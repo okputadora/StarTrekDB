a0b065a8a0175d8df6d7526b2562ad411c126736
okputadora <okputadora@gmail.com>
2016-11-30 14:55:30 -0500

    Basic layout for homepage

---
+++ stdbwp/homepage.php
@@ -0,0 +1,82 @@
+<html lang="en" dir="ltr" xmlns="http://www.w3.org/1999/xhtml">
+   <head>
+     <meta http-equiv="content-type" content="text/xml; charset=utf-8" />
+     <!--ensures proper rendering and touch zooming on mobile devices-->
+     <meta name="viewport" content="width=device-width, initial-scale=1">
+     <title>StarTrek</title>
+     <!--import google font-->
+     <link href="https://fonts.googleapis.com/css?family=Bungee|Roboto" rel="stylesheet">
+     <!--import style sheet-->
+     <link rel="stylesheet" type = "text/css"
+     href=homepagestyle.css>
+     <!--link javascript-->
+  </head>
+
+  <body>
+    <div id ="navbar-fixed">
+      <div id="navbar">
+        <div id="navTitle">
+          Star Trek WatchList
+        </div>
+        <div id="navTabs">
+          <div id = "navTags">Tags</div>
+          <div id = "navSeries">Series</div>
+          <div id = "navBestOf">Best Of</div>
+          <div id = "navSearch">Search</div>
+          <div id = "signIn">Sign In</div>
+        </div>
+      </div>
+      </div>
+
+    <div class="header">
+      <div id="headerimg">
+        <img src = "imgs/enterprise_d.jpg" width = "100%" />
+      </div>
+    </div>
+    <div class="main-cont"id="tags">
+      <h2>Tags</h2>
+      <div class="sub-cont" id="tag-cont">
+        <div id="tagList">
+          <div id="timeTag">Time</div>
+          <div id="bTimeTag">Back In Time</div>
+          <div id="pdTag">What Prime Directive?</div>
+          <div id="holoTag">Holodeck</div>
+          <div id="mutinyTag">Mutiny</div>
+          <div id="mindRapeTag">Mind Rape</div>
+          <div id="psTag">Primitive Species</div>
+          <div id="fcTag">First Contact</div>
+          <div id="earthTag">On Earth</div>
+          <div id="earthTag">On Earth</div>
+          <div id="searchTags">Search</div>
+        </div>
+        <div id="tagResults">
+
+        </div>
+      </div>
+    </div>
+    <div class="main-cont"id="series">
+      <h2>Series</h2>
+      <div class="sub-cont"id="seriesList">
+        <div id="ogSeries">
+          <h3>The Original Series</h3>
+          <img src="imgs/og_crew.jpg" />
+        </div>
+        <div id="tngSeries">
+          <h3>The Next Generation</h3>
+          <img src="imgs/tng_crew.jpg" />
+        </div>
+        <div id="ds9Series">
+          <h3>Deep Space Nine</h3>
+          <img src="imgs/ds9_crew.jpg" />
+        </div>
+        <div id="vgrSeries">
+          <h3>Voyager</h3>
+          <img src="imgs/vgr_crew.jpg" />
+        </div>
+      </div>
+    </div>
+    <div class="main-cont"id="series">
+      <h2>Best Of</h2>
+    </div>
+
+  </body>
